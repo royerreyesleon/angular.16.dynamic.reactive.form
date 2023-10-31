222d528dd2cb9bb1eaaffc8f15985d85c39ca844
rreyes <rreyes@ips-mexico.com>
2023-10-31 16:19:14 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'dynamic-form';
+export class AppComponent implements OnInit {
+  public form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.onInitForm();
+  }
+
+  ngOnInit(): void {
+    this.addItemDynamic();
+  }
+
+  private onInitForm() {
+    this.form = this.formBuilder.group({
+      username: new FormControl('admin', [Validators.required]),
+      password: new FormControl('admin', [Validators.required]),
+      emails: new FormArray([], [Validators.required]),
+    });
+  }
+
+  public get formEmails(): FormArray {
+    return this.form.get('emails') as FormArray;
+  }
+
+  public addItemDynamic() {
+    const email = this.formBuilder.group({
+      email: new FormControl('test@test.com', [
+        Validators.required,
+        Validators.email,
+      ]),
+    });
+
+    this.formEmails.push(email);
+  }
+
+  public deleteItemFormEmails(index: number) {
+    this.formEmails.removeAt(index);
+  }
+
+  public onSubmit() {
+    alert('form is valid');
+  }
 }
